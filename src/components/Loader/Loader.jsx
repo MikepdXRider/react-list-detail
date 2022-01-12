e913b3d248afba8796954d32a8d6984325fb3ad2
Michael Rider <mikepdxrider@gmail.com>
2022-01-12 08:05:26 -0800

    bebe change

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './Loader.css'
 
+// comment for github
 export default function Loader() {
     return (
         <article className={styles.loaderContainer}>
