a1b068900a1efc5cca74a01359e4a3699b932a44
Michael Rider <mikepdxrider@gmail.com>
2022-01-12 09:09:11 -0800

    removes comment

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import styles from './Loader.css'
 
-// comment for github
 export default function Loader() {
     return (
         <article className={styles.loaderContainer}>
